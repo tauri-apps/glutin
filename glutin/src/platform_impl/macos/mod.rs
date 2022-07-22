e031a932231576dbf430c1a1bdeb64bff8d2e8d4
Wu Yu Wei <wusyong9104@gmail.com>
2022-07-22 15:20:27 +0800

    Replace winit with tao

--- glutin/src/platform_impl/macos/mod.rs
+++ glutin/src/platform_impl/macos/mod.rs
@@ -52,7 +52,7 @@ impl Context {
         pf_reqs: &PixelFormatRequirements,
         gl_attr: &GlAttributes<&Context>,
     ) -> Result<(Window, Self), CreationError> {
-        let transparent = wb.transparent();
+        let transparent = wb.window.transparent;
         let win = wb.build(el)?;
 
         let share_ctx = gl_attr.sharing.map_or(nil, |c| *c.get_id());
