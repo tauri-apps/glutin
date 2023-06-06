64367f853f1abdd81ac1cedbb941f0f0252f2f2f
DK Liao <dklassic@gmail.com>
2023-06-06 16:59:07 +0800

    Remove android example

--- glutin_examples/examples/android.rs
+++
@@ -1,9 +0,0 @@
-#![cfg(android_platform)]
-
-use winit::event_loop::EventLoop;
-use winit::platform::android::EventLoopBuilderExtAndroid;
-
-#[no_mangle]
-fn android_main(app: winit::platform::android::activity::AndroidApp) {
-    glutin_examples::main(EventLoop::new())
-}
