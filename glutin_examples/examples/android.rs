d349e426613f05f8638bbf7169a55769af41132b
DK Liao <dklassic@gmail.com>
2023-06-06 16:17:48 +0800

    Add Gtk Install in CI

--- glutin_examples/examples/android.rs
+++ glutin_examples/examples/android.rs
@@ -1,10 +1,9 @@
 #![cfg(android_platform)]
 
-use winit::event_loop::EventLoopBuilder;
+use winit::event_loop::EventLoop;
 use winit::platform::android::EventLoopBuilderExtAndroid;
 
 #[no_mangle]
 fn android_main(app: winit::platform::android::activity::AndroidApp) {
-    let event_loop = EventLoopBuilder::new().with_android_app(app).build();
-    glutin_examples::main(event_loop)
+    glutin_examples::main(EventLoop::new())
 }
